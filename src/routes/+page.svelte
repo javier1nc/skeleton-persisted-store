63a4bcfcd848b1e242d195b69ba5ea0281fceccd
Javier Sánchez Mendoza <javier.sanchezm@sat.gob.mx>
2024-08-28 11:05:07 -0600

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+// https://www.npmjs.com/package/svelte-persisted-store
+
+import { get } from 'svelte/store'
+import { preferences } from './stores'
+
+preferences.set({
+    theme: 'ligth',
+    pane: '100%',
+  }) // set value
+
+$: console.log(get(preferences))
+</script>
+
